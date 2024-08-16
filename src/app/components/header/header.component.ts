45700c45a77c03b508cb518429af3603612e45a4
Kyrylo Shevchenko <kir971@gmail.com>
2024-08-16 23:35:37 +0300

    View current rate

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,20 +4,27 @@ import { Store } from '@ngrx/store';
 import { loadCurrentRate } from '../../store/actions';
 import { selectCurrentRate } from '../../store/selectors';
 import { AsyncPipe } from '@angular/common';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, AsyncPipe],
+  imports: [MatToolbarModule, AsyncPipe, MatSelectModule, MatFormFieldModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent implements OnInit {
+  public defaultCurrency = 'USD';
   public selectedCurrentRate$ = this.store.select(selectCurrentRate);
 
   constructor(private store: Store) {}
 
   ngOnInit(): void {
-    this.store.dispatch(loadCurrentRate({ base: 'USD', target: 'UAH' }));
+    this.store.dispatch(loadCurrentRate({ base: this.defaultCurrency, target: 'UAH' }));
+  }
+
+  onChangePopularCurrency(event: MatSelectChange) {
+    this.store.dispatch(loadCurrentRate({ base: event.value, target: 'UAH' }));
   }
 }
