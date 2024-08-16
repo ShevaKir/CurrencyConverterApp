2531b328e2931d8df77f0d36cc1aaf4ade804580
Kyrylo Shevchenko <kir971@gmail.com>
2024-08-17 01:09:02 +0300

    Added the supported currency codes

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { ExchangeRateResponse } from '../models/exchange-rate-response.model';
+import { CurrencyCode } from '../models/currency-code.model';
 
 @Injectable({
   providedIn: 'root',
@@ -17,6 +18,21 @@ export class CurrencyService {
   ): Observable<ExchangeRateResponse> {
     return this.http.get<ExchangeRateResponse>(
       `${this._apiPath}/pair/${base.toUpperCase()}/${target.toUpperCase()}`
+    );
+  }
+
+  getSupportedCurrencies(): Observable<CurrencyCode[]> {
+    return this.http.get<any>(`${this._apiPath}/codes`).pipe(
+      map((response) => {
+        const supportedCodes: CurrencyCode[] = response.supported_codes.map(
+          (item: [string, string]) => ({
+            code: item[0],
+            name: item[1],
+          })
+        );
+
+        return supportedCodes;
+      })
     );
   }
 }
